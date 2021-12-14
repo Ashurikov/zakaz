a819fcc423675ac62de897e47b85f616f6b6ef31
Shurykau Andrei <95248508+Ashurikov@users.noreply.github.com>
2021-12-14 20:01:35 +0300

    v.1

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require ('express')
+const mongoose = require ('mongoose')
+const path =require ('path')
+const exphbs = require ('express-handlebars')
+const todoRoutes = require ('./routes/todos')
+
+const PORT = process.env.PORT ||3000
+
+const app = express()
+const hbs = exphbs.create ({
+    defaultLayout: 'main',
+    runtimeOptions: {
+              allowProtoPropertiesByDefault: true,
+              allowProtoMethodsByDefault: true,
+    },
+        
+    extname: 'hbs'
+})
+
+app.engine ('hbs', hbs.engine)
+app.set ('view engine','hbs')
+app.set ('views', 'views')
+
+app.use (express.urlencoded ({extended:true}))
+app.use (express.static(path.join (__dirname, 'public')))
+
+app.use (todoRoutes)
+
+
+
+async function start () {
+    try {
+       await mongoose.connect ('mongodb+srv://Ashurikov:1q2w3e4r@cluster0.snn4n.mongodb.net/As',{
+        useNewURlParser: true,
+        useUnifiedTopology: true
+       
+        })
+        app.listen(PORT, () => {
+            console.log ('Start')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+  }
+
+start()
